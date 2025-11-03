a5a164cbd41edf9044dc813ead7602e36bc2fab6
JuanFranciscoAp <jfug0707@gmail.com>
2025-11-03 07:29:42 -0700

    Se agrega CRUD de CheckLIst

--- conexion.php
+++ conexion.php
@@ -7,12 +7,23 @@ class Conexion{
             $conn = new PDO("mysql:host=localhost;dbname=bdsaturno",
             "root","clave",array(PDO::MYSQL_ATTR_INIT_COMMAND =>
              "SET NAMES utf8"));
-          
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
             return $conn;
         }
         catch (PDOException $e) {
             echo 'Falló la conexión: ' . $e->getMessage();
+            return null;
         }
 
     }
+}
+// Prueba de conexión
+
+
+if (!Conexion::conectar()) {
+    echo json_encode(['success' => false, 'message' => 'Error de conexión']);
+    exit();
+} else {
+    echo json_encode(['success' => true, 'message' => 'Conexión exitosa']);
 }
